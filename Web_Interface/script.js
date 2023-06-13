5ef52c5ff4278995ae7863e75cfcaa62a9aea243
Christysss <49929520+Christysss@users.noreply.github.com>
2023-06-12 23:38:37 -0400

    home page

---
+++ Web_Interface/script.js
@@ -0,0 +1,8 @@
+const startBtn = document.querySelector('.start-btn');
+const questionSection = document.querySelector('.question-section');
+
+
+startBtn.onclick = () => {
+    startBtn.classList.add('active')
+    // main.classList.remove('active');
+}
