f28414306501fe3e13a62ddefdd2a3184efec5ee
Christysss <49929520+Christysss@users.noreply.github.com>
2023-06-23 00:36:23 -0400

    cannot write csv

--- Web_Interface/backend.py
+++ Web_Interface/backend.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import csv
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
@@ -13,7 +14,13 @@ app = FastAPI()
 app.mount("/css", StaticFiles(directory= css_directory), name="css")
 app.mount("/images", StaticFiles(directory=imgs_directory), name="images")
 
-# csv_filepath = Path(__file__).parent / "form_data.csv"
+# # Allow cross-origin resource sharing (CORS)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 class FormData(BaseModel):
     location: str
@@ -23,21 +30,63 @@ class FormData(BaseModel):
     rent_deposit: str
     checkbox: list[str] = []
 
-@app.post("/submit-form")
-def submit_form(data: FormData):
-    # Create a dictionary with the form data
+# @app.post("/submit-form")
+# def submit_form(data: FormData):
+#     # Create a dictionary with the form data
     
     
-    print(data)
+#     print("Hello World")
     
-    # Save the form data to a CSV file
-    with open("form_data.csv", mode="w", newline="") as file:
-        writer = csv.writer(file)#, fieldnames=data.__fields__.keys()
-        writer.writerow(
-            [data.location, data.l_represented, data.length, data.rent_deposit, ",".join(data.checkbox)])
+#     # Save the form data to a CSV file
+#     with open("form_data.csv", mode="w", newline="") as file:
+#         writer = csv.writer(file)#, fieldnames=data.__fields__.keys()
+#         writer.writerow(
+#             [data.location, data.l_represented, data.length, data.rent_deposit, ",".join(data.checkbox)])
     
-    return pass_file("result.html")
+#     return {}
 
+# @app.post("/submit-form")
+# async def submit_form(data: dict):
+#     # Process the received form data
+#     location = data.get('location')
+#     l_represented = data.get('l_represented')
+#     length = data.get('length')
+#     rent = data.get('rent')
+#     rent_deposit = data.get('rent_deposit')
+#     checkbox = data.get('checkbox')
+
+#     print("Hellloooooo")
+#     # Save the form data to a CSV file
+#     with open('form_data.csv', mode='a', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow([location, l_represented, length, rent,rent_deposit,checkbox])
+
+#     return {"message": "Form submitted successfully"}
+
+@app.post("/submit-form")
+async def submit_form(request: Request):
+    form_data = await request.form()
+    data = dict(form_data)
+
+    print(data)
+    # Process the received form data
+    location = data.get('location')
+    l_represented = data.get('l_represented')
+    length = data.get('length')
+    rent = data.get('rent')
+    rent_deposit = data.get('rent_deposit')
+    checkbox = data.get('checkbox')
+
+    # Save the form data to a CSV file
+    csv_data = [location, l_represented, length, rent,rent_deposit,checkbox]
+    save_to_csv(csv_data)
+
+    return RedirectResponse(url="/result.html")
+
+def save_to_csv(data):
+    with open("form_data.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
 
 @app.get("/")
 async def start():
